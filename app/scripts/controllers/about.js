d6c3e1dffa9fb0ab05dd4c6237133c38430b944c
Richard Min <richardmin97@gmail.com>
2016-03-29 09:53:29 -0700

    initial yeoman commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dargonApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dargonApp
+ */
+angular.module('dargonApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dargonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dargonApp
+ */
+angular.module('dargonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
